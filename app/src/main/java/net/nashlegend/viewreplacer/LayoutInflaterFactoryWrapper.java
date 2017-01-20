d81046cec7201b15f23f63b8edb908ec128d4502
NashLegend <panzhihuipzh@gmail.com>
2017-01-20 16:27:32 +0800

    more safer way

---
+++ app/src/main/java/net/nashlegend/viewreplacer/LayoutInflaterFactoryWrapper.java
@@ -0,0 +1,63 @@
+package net.nashlegend.viewreplacer;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.LayoutInflaterFactory;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewParent;
+import android.view.Window;
+
+/**
+ * @author 潘志会 @ Zhihu Inc.
+ * @since 01-20-2017
+ */
+
+public class LayoutInflaterFactoryWrapper implements LayoutInflaterFactory {
+	private ReplaceInflater mInflater = new ReplaceInflater();
+	private LayoutInflaterFactory originalFactory;
+	private Window mWindow;
+
+	public LayoutInflaterFactoryWrapper(LayoutInflaterFactory originalFactory, Window window) {
+		this.originalFactory = originalFactory;
+		mWindow = window;
+	}
+
+	@Override
+	public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+		View view = replaceView(parent, name, context, attrs);
+		if (view == null) {
+			view = originalFactory.onCreateView(parent, name, context, attrs);
+		}
+		return view;
+	}
+
+
+	private View replaceView(View parent, String name, Context context, AttributeSet attrs) {
+		final boolean isPre21 = Build.VERSION.SDK_INT < 21;
+		// 5.0之前的且父控件不为空的且所在View树没有添加到Window上的会继承父控件的Context
+		final boolean inheritContext = isPre21 && shouldInheritContext((ViewParent) parent);
+		return mInflater.createView(parent, name, context, attrs, inheritContext, isPre21);
+	}
+
+	/**
+	 * 如果View所在的树并没有加到Window上，则返回true
+	 */
+	private boolean shouldInheritContext(ViewParent parent) {
+		if (parent == null) {
+			return false;
+		}
+		final View windowDecor = mWindow.getDecorView();
+		while (true) {
+			if (parent == null) {
+				return true;
+			} else if (parent == windowDecor
+				|| !(parent instanceof View)
+				|| ViewCompat.isAttachedToWindow((View) parent)) {
+				return false;
+			}
+			parent = parent.getParent();
+		}
+	}
+}
--- app/src/main/java/net/nashlegend/viewreplacer/MainActivity.java
+++ app/src/main/java/net/nashlegend/viewreplacer/MainActivity.java
@@ -1,7 +1,11 @@
 package net.nashlegend.viewreplacer;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.LayoutInflater.Factory2;
 import java.lang.reflect.Field;
@@ -9,7 +13,16 @@ import java.lang.reflect.Field;
 public class MainActivity extends AppCompatActivity {
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
+		installViewFactoryBefore();
 		super.onCreate(savedInstanceState);
+		//injectWithReflect();
+		setContentView(R.layout.activity_main);
+	}
+
+	/**
+	 * 通过反射的方式设置Factory2
+	 */
+	private void injectWithReflect() {
 		try {
 			Factory2 factory2 = getLayoutInflater().getFactory2();
 			if (factory2 != null) {
@@ -21,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		setContentView(R.layout.activity_main);
+	}
+
+	/**
+	 * 	通过抢先设置Factory的方式，要在super.onCreate(savedInstanceState)之前调用，应该用这个更安全
+	 */
+	public void installViewFactoryBefore() {
+		AppCompatDelegate delegate = getDelegate();
+		if (delegate instanceof LayoutInflaterFactory) {
+			LayoutInflaterFactory factory = (LayoutInflaterFactory) delegate;
+			LayoutInflaterFactoryWrapper wrapper = new LayoutInflaterFactoryWrapper(factory, getWindow());
+			LayoutInflater layoutInflater = LayoutInflater.from(this);
+			if (layoutInflater.getFactory() == null) {
+				LayoutInflaterCompat.setFactory(layoutInflater, wrapper);
+			}
+		}
 	}
 }
